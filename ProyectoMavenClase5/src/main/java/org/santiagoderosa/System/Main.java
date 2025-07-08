27afdfbf728be8ea7717a37ed8397574f45ff95f
SERV-DRV <sderosa-2023220@kinal.edu.gt>
2025-07-08 11:12:59 -0600

    Summit Proyect Kinal Familiar
    Add Proyect in the repository

---
+++ ProyectoMavenClase5/src/main/java/org/santiagoderosa/System/Main.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.santiagoderosa.System;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author informatica
+ */
+public class Main extends Application{
+
+    public static void main(String[] args) {
+        launch(args);
+//        JSONObject persona = new JSONObject();
+//            persona.put("nombre:","Santiago");
+//            persona.put("apellido:","De Rosa");
+//            persona.put("edad:", 17 );
+//            persona.put("valido:",true);
+//            
+//            System.out.println("Contenido de JSON:");
+//            System.out.println(persona.toString(1));
+    }
+
+    @Override
+    public void start(Stage escenario) throws Exception {
+       FXMLLoader cargador = new FXMLLoader(getClass().getResource("/view/InicioView.fxml"));
+       Parent raiz = cargador.load();
+       Scene escena = new Scene(raiz);
+       escenario.setScene(escena);
+       escenario.show();
+    }
+}
