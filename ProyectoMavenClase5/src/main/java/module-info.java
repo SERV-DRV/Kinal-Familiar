9a93e518607b8245836fa37b7e9e36f6ccd2e70a
ksolo-2022439 <ksolorzano-2022439@kinal.edu.gt>
2025-07-11 16:01:18 -0600

    module-info agregado

---
+++ ProyectoMavenClase5/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.kinalfamiliar.proyectomavenclase5 {
+    requires java.sql;
+    requires java.logging;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.json;
+    requires mysql.connector.java;
+
+    opens org.kinalfamiliar.system to javafx.fxml;
+    opens org.kinalfamiliar.controller to javafx.fxml;
+    opens org.kinalfamiliar.model to javafx.base;
+    
+    exports org.kinalfamiliar.system;
+}
